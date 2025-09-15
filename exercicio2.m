670f0979884953d74ce24df92e037d3a0cec2397
dudakbs <eduardakb@hotmail.com>
2025-09-15 11:10:58 -0300

    Update exercicio2.m

--- exercicio2.m
+++ exercicio2.m
@@ -9,10 +9,24 @@ pkg load optim
 % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
+ NG = 4;
+ %f = @(x) (10*x(1) + (2*NG)*x(2));
+ f = [10, 2*NG]';
 
-% digite seu codigo aqui
+ x0 = [1,1]';
 
+ A = [-0.30, -0.20; -0.20, -0.25; -0.25, -0.30];
+ b = [-15; -10; -12];
+ Aeq = [];
+ beq= [];
 
+ ub=[20;50];
+ lb = [0; 0];
+
+ x = linprog(f, A, b, Aeq, Beq, lb, ub);
+
+ x = x';
+ 
 % mantenha essas duas linhas finais
 blendA = x(1);
 blendB = x(2);
